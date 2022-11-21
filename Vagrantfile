x-shell-codes/nginx

Vagrantfile added in project.

Vagrantfile
@@ -1,0 +1,9 @@
+Vagrant.configure("2") do |config|
+	config.vm.box = "ubuntu/jammy64"
+
+	config.vm.provider "virtualbox" do |vb|
+		vb.name = "nginx.local.x-shell.codes"
+		vb.cpus = 1
+		vb.memory = 4096
+	end
+end

x-shell-codes/postfix

Vagrantfile
@@ -1,0 +1,9 @@
+Vagrant.configure("2") do |config|
+	config.vm.box = "ubuntu/jammy64"
+
+	config.vm.provider "virtualbox" do |vb|
+		vb.name = "postfix.local.x-shell.codes"
+		vb.cpus = 1
+		vb.memory = 4096
+	end
+end